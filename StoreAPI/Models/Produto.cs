d306546cf89a7f056a5f847303298f584daf89c7
vanessa Carvalho <vnscarvalh@gmail.com>
2026-04-22 19:49:51 -0300

    Criacao das models Categoria e Produtos

---
+++ StoreAPI/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿
+namespace StoreAPI.Models;
+
+public class Produto
+{
+    public int ProdutoId { get; set; }
+    public string? Nome { get; set; }
+    public string? Descricao { get; set; }
+    public decimal Preco { get; set; }
+    public float Estoque { get; set; }
+    public string? ImagemUrl { get; set; }
+    public DateTime DataCadastro { get; set; }
+
+}
